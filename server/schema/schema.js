fefbe16a89411bd7bc9e15a5996bcffb3c1572ec
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-02-11 13:10:06 +0300

    Defined Book schema

---
+++ server/schema/schema.js
@@ -0,0 +1,12 @@
+const graphql = reauire('graphql');
+
+const { GraphQLObjectType, GraphQLString } = graphql;
+
+const BookType = new GraphQLObjectType({
+  name: 'Book',
+  fields: () => ({
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    genre: { type: GraphQLString },
+  }),
+});
