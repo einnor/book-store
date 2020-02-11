7b522b5fb82c7e9c14d56f0aad6b5d7fba6c4613
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-02-11 13:19:11 +0300

    Defined root query for getting book

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,6 +1,6 @@
 const graphql = reauire('graphql');
 
-const { GraphQLObjectType, GraphQLString } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
 
 const BookType = new GraphQLObjectType({
   name: 'Book',
@@ -9,4 +9,23 @@ const BookType = new GraphQLObjectType({
     name: { type: GraphQLString },
     genre: { type: GraphQLString },
   }),
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    book: {
+      type: BookType,
+      args: {
+        id: { type: GraphQLString },
+      },
+      resolve (parent, args) {
+        // Code to get data from DB / Other source
+      }
+    },
+  },
+});
+
+module.exports = GraphQLSchema({
+  query: RootQuery,
 });
