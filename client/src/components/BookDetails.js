ac2a220c16dd3559e6c02e107aa360bb4ec21c1d
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-02-12 22:38:49 +0300

    Render book details

--- client/src/components/BookDetails.js
+++ client/src/components/BookDetails.js
@@ -6,9 +6,24 @@ const { getBookQuery } = '../queries/queries';
 const BookDetails = ({ bookId }) => {
   const { loading, error, data } = useQuery(getBookQuery, { variables: { id: bookId }});
 
+  const { book } = data;
   return (
     <div className="book-details">
-
+      {
+        book ? (
+          <div>
+            <h2>{book.name}</h2>
+            <p>{book.genre}</p>
+            <p>{book.author.name}</p>
+            <p>All books by this author</p>
+            <ul className="other-books">
+              {book.author.books.map((book) => (
+                <li key={book.id}>{book.name}</li>
+              ))}
+            </ul>
+          </div>
+        ) : null
+      }
     </div>
   )
 }
