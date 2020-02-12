2cb2dafd997378e8e3494abc1c87df0772789560
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-02-12 16:45:56 +0300

    Book list skeleton component

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
+
+import BookList from './components/BookList';
 import './App.css';
 
 function App() {
   return (
     <div className="main">
-
+      <h1>Online Book Store</h1>
+      <BookList />
     </div>
   );
 }
---
+++ client/src/components/BookList.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const BookList = () => {
+  return (
+    <div>
+      <ul className="book-list">
+        <li>Book Name</li>
+      </ul>
+    </div>
+  )
+}
+
+export default BookList;
