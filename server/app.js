3a9c7ef22a58c61f73f78944f7a0b8a01aacaf30
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-02-12 15:21:20 +0300

    Connect to mLab

--- server/app.js
+++ server/app.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
+const mongoose = require('mongoose');
 
 const schema = require('./schema/schema');
 
+require('dotenv').config();
+
 const app = express();
+
+mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error'));
+db.once('open', () => {
+  // We're connected
+  console.log('Connected to the database');
+});
 
 app.use('/graphql', graphqlHTTP({
   schema,
