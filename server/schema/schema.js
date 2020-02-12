c2aa12fa24321c5a42a44be742f4c5a7a5319d15
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-02-12 12:58:42 +0300

    Type relations between author and book

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -10,9 +10,9 @@ const {
 
 // Dummy data
 const books = [
-  { name: 'Name of the Wind', genre: 'Fantasy', id: '1' },
-  { name: 'The Final Empire', genre: 'Fantasy', id: '2' },
-  { name: 'The Long Earth', genre: 'Sci-Fi', id: '3' },
+  { name: 'Name of the Wind', genre: 'Fantasy', id: '1', authorId: '1' },
+    { name: 'The Final Empire', genre: 'Fantasy', id: '2', authorId: '2' },
+    { name: 'The Long Earth', genre: 'Sci-Fi', id: '3', authorId: '3' },
 ];
 
 const authors = [
@@ -27,6 +27,12 @@ const BookType = new GraphQLObjectType({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
     genre: { type: GraphQLString },
+    author: {
+      type: AuthorType,
+      resolve (parent, args) {
+        return authors.find((author) => author.id === parent.authorId);
+      }
+    }
   }),
 });
 
