c931bdefe18bbcc1ed84c92b52772d3de9733b6d
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-02-12 18:48:26 +0300

    track state of form

--- client/src/components/AddBook.js
+++ client/src/components/AddBook.js
@@ -1,11 +1,20 @@
-import React from 'react';
-import { gql } from 'apollo-boost';
+import React, { useState } from 'react';
 import { useQuery } from '@apollo/react-hooks';
 
 import { getAuthorsQuery } from '../queries/queries';
 
 const AddBook = () => {
   const { loading, error, data } = useQuery(getAuthorsQuery);
+  const [state, setState] = useState({
+    name: '',
+    genre: '',
+    authorId: '',
+  });
+
+  const onInputChange = (e) => {
+    const { name, value } = e.target;
+    setState({ ...state, [name]: value });
+  };
 
   const displayAuthors = () => {
     if(loading){
@@ -13,20 +22,21 @@ const AddBook = () => {
     } else {
       return data.authors.map((author) => (<option key={author.id} value={author.id}>{author.name}</option>));
     }
-}
+  }
+
   return (
     <form className="add-book">
       <div className="field">
           <label>Book name:</label>
-          <input type="text" />
+          <input type="text" name="name" onChange={onInputChange} />
       </div>
       <div className="field">
           <label>Genre:</label>
-          <input type="text" />
+          <input type="text" name="genre" onChange={onInputChange} />
       </div>
       <div className="field">
           <label>Author:</label>
-          <select>
+          <select name="authorId" onChange={onInputChange}>
               <option>Select author</option>
               {displayAuthors()}
           </select>
--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { gql } from 'apollo-boost';
 import { useQuery } from '@apollo/react-hooks';
 
 import { getBooksQuery } from '../queries/queries';
