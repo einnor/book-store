bcbbe897fa67f56e4d4a6b898b06bc1215b39e2f
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-02-12 15:10:10 +0300

    Fetch all authors

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -82,6 +82,12 @@ const RootQuery = new GraphQLObjectType({
       resolve (parent, args) {
         return books;
       }
+    },
+    authors: {
+      type: new GraphQLList(AuthorType),
+      resolve (parent, args) {
+        return authors;
+      }
     }
   },
 });
